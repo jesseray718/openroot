237d04f63704fd4ee6ef5e78df7dc68325f06e42
jesseray718 <jesse@openroot.org>
2026-07-31 22:16:58 -0500

    aider: add a comment about Landauer limit

--- scripts/test_ots.sh
+++ scripts/test_ots.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 # Test script for hello world function
+# Note: This script's energy efficiency is limited by the Landauer limit, which is the minimum energy required to erase one bit of information (approximately 2.9 * 10^-21 J at room temperature)
 counter=0
 start_time=$(date +%s)
 get_system_uptime() {
