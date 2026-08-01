9440fc846a0fe7378d949f318638072cbe2a4969
jesseray718 <jesse@openroot.org>
2026-07-31 19:37:09 -0500

    aider: add a counter variable

--- scripts/test_ots.sh
+++ scripts/test_ots.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e
 # Test script for hello world function
+counter=0
 hello_world() {
   # This script prints a greeting message
-  echo "hello world"
+  ((counter++))
+  echo "hello world $counter"
 }
 
 hello_world
