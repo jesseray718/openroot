a7fbd59012b2bc8c072f2c837fe0a092b43d5e20
jesseray718 <jesse@openroot.org>
2026-07-31 19:54:09 -0500

    aider: add a uptime counter that prints seconds since boot

--- scripts/test_ots.sh
+++ scripts/test_ots.sh
@@ -2,10 +2,12 @@
 set -e
 # Test script for hello world function
 counter=0
+start_time=$(date +%s)
 hello_world() {
   # This script prints a greeting message
   ((counter++))
-  echo "hello world $counter"
+  uptime=$(( $(date +%s) - start_time ))
+  echo "hello world $counter, uptime: $uptime seconds"
 }
 
 hello_world
