baa0bac759ebd1b13ff0267c67ddb4d597c7d95c
jesseray718 <jesse@openroot.org>
2026-07-31 21:14:16 -0500

    aider: add a function that returns system uptime in seconds

--- scripts/test_ots.sh
+++ scripts/test_ots.sh
@@ -3,11 +3,16 @@ set -e
 # Test script for hello world function
 counter=0
 start_time=$(date +%s)
+get_system_uptime() {
+  # This function returns system uptime in seconds
+  uptime=$(cat /proc/uptime | cut -d ' ' -f 1)
+  echo "$uptime"
+}
+
 hello_world() {
   # This script prints a greeting message
   ((counter++))
-  uptime=$(( $(date +%s) - start_time ))
-  echo "hello world $counter, uptime: $uptime seconds"
+  echo "hello world $counter, system uptime: $(get_system_uptime) seconds, script uptime: $(( $(date +%s) - start_time )) seconds"
 }
 
 while true
